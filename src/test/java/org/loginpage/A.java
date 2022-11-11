75833b2097e74691588c888aefe84a9264bd79c9
saral7200 <krsaralkumar@gmail.com>
2022-11-11 14:44:53 +0530

    new branch

---
+++ src/test/java/org/loginpage/A.java
@@ -0,0 +1,12 @@
+package org.loginpage;
+
+public class A {
+	private void sysout() {
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		System.out.println("S");
+	}
+
+}
